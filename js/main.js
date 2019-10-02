38509315e6af3ac09ff7be891bee68d176ecb658
Pavel <pav-kovrizhko@mail.ru>
2019-10-02 22:22:24 +0300

    locationX corrected

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@
   var getOffers = function (count) {
     var offersList = [];
     for (var i = 0; i < count; i++) {
-      var locationX = getRandomNumber(PIN_WIDTH, MAP_WIDTH - PIN_WIDTH);
+      var locationX = getRandomNumber(0 + PIN_WIDTH / 2, MAP_WIDTH - PIN_WIDTH / 2);
       var locationY = getRandomNumber(130, 630);
       var offer = {
         'author': {
